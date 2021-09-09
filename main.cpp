6cfce08c894a6623b27a838d9b55bac713dbaaae
Steven <steven928@hotmail.fr>
2021-09-09 11:36:08 +0200

    working

--- main.cpp
+++ main.cpp
@@ -1,75 +1,68 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <iostream>
-
-
-
-
+#include <iterator>
+#include <sstream>
 
 
 using namespace std;
 using namespace cv;
 
+Mat rotate(Mat src, double angle)   //rotate function returning mat object with parametres imagefile and angle    
+{
+    Mat dst;      //Mat object for output image file
+    Point2f pt(src.cols/2., src.rows/2.);          //point from where to rotate    
+    Mat r = getRotationMatrix2D(pt, angle, 1.0);      //Mat object for storing after rotation
+    warpAffine(src, dst, r, Size(src.cols, src.rows));  ///applie an affine transforation to image.
+    return dst;         //returning Mat object for output image file
+}
+
+vector<vector<string>> readText(string filename){
+   string line;
+   ifstream fin( filename ); assert( fin );
+   vector<vector<string>> list;
+   while( getline( fin, line ) )
+   {
+	stringstream ss(line);
+	istream_iterator<string> begin(ss);
+	istream_iterator<string> end;
+
+	vector<string> frags(begin,end);
+	// copy(frags.begin(), frags.end(), ostream_iterator<string>(cout, "\n"));
+	list.push_back(frags);
+   }
+   
+   return list;
+}
 
 
 int main(int argc, char** argv){
 
 	Mat imageIn = imread( argv[1], IMREAD_GRAYSCALE );
-
-	Mat img1 = imread( "./frag_eroded/frag_eroded_0.png" );
-
-	Mat DispImage = Mat::zeros(Size(1920,1080), CV_8UC3);
+	Mat fragment;
+	Rect ROI;
+	Mat DispImage = Mat::zeros(Size(1920,900), CV_8UC3);
 	
-	
-	if(! img1.data )                              // Check for invalid input
-    {
+	vector<vector<string>> grid = readText("fragments.txt");
+	for ( auto row : grid )
+	{
+		fragment = imread( "./frag_eroded/frag_eroded_"+row[0]+".png" );
+		if(! fragment.data )
+    	{
         	cout <<  "Could not open or find the image" << std::endl ;
         	return -1;
-   	}
+   		}
+ 		Rect ROI(stoi(row[1]), stoi(row[2]),(int)( fragment.rows ), (int)( fragment.cols ));
+		Mat temp;
+		resize(fragment,temp, Size(ROI.width, ROI.height));
+		temp = rotate(temp, stod(row[3]));
+		temp.copyTo(DispImage(ROI));
 
-	Rect ROI(10, 20, (int)( 200 ), (int)( 300 ));
-    Mat temp;resize(img1,temp, Size(ROI.width, ROI.height));
-    temp.copyTo(DispImage(ROI));
+	}
 	
-	
-	imshow( "Display window", DispImage );             
+	imshow( "Display window", DispImage );  
+	          
 	waitKey(0);       	
 	
 	return 0;
-}
-
-
-
-void ShowManyImages(string title) {
-int size;
-int i;
-int x, y;
-int nArgs = 5;
-
-Mat DispImage = Mat::zeros(Size(1920,1080), CV_8UC3);
-
-va_list args;
-
-
-for (i = 0; i < nArgs; i++) {
-
-    Mat img = va_arg(args, Mat);
-
-    if(img.empty()) {
-        printf("Invalid arguments");
-        return;
-    }
-
-    Rect ROI(100, 130, (int)( 200 ), (int)( 300 ));
-    Mat temp; resize(img,temp, Size(ROI.width, ROI.height));
-    temp.copyTo(DispImage(ROI));
-}
-
-
-namedWindow( title, 1 );
-imshow( title, DispImage);
-waitKey();
-
-
-va_end(args);
 }
